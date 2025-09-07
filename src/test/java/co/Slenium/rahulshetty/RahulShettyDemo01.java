10e40c6b425beeb5523a1356a99fd29ebafdae1d
Prasad Muske <prasadmuske4871@gmail.com>
2025-09-08 01:36:31 +0530

    first commit 8Sept2025 1:36 AM

---
+++ src/test/java/co/Slenium/rahulshetty/RahulShettyDemo01.java
@@ -0,0 +1,28 @@
+package co.Slenium.rahulshetty;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class RahulShettyDemo01 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("inputUsername")).sendKeys("Rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.className("signInBtn")).click();
+		//driver.findElement(By.linkText("Sign In")).click();
+		//driver.findElement(By.partialLinkText("Sign In")).click();
+		Thread.sleep(5000);
+		System.out.println(driver.findElement(By.tagName("h1")).getText());
+		Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(), "Welcome to Rahul Shetty Academy");
+		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+		driver.close();
+
+	}
+}
---
+++ src/test/java/co/Slenium/rahulshetty/RahulShettyDemo03_SuggestiveDropdown.java
@@ -0,0 +1,56 @@
+package co.Slenium.rahulshetty;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RahulShettyDemo03_SuggestiveDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		//driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.get("https://www.spicejet.com/");
+		driver.manage().window().maximize();
+		
+		
+		// driver.findElement(By.id("autocomplete")).sendKeys("ind");
+		 
+		driver.findElement(By.xpath("//div[text()='From']/parent::div/div[2]/input")).sendKeys("su");
+		
+		List<WebElement> options=driver.findElements(By.xpath("//ul[@id='ui-id-1']/li"));
+		
+		
+		System.out.println(options.size());
+		for(WebElement option:options) {
+			if(option.getText().equalsIgnoreCase("Surat"))
+			{
+				option.click();
+				break;
+			}
+			
+		}
+		
+		
+		// Handling Checkbox
+		
+		/*
+		//driver.findElement(By.xpath("//input[@value='option2']")).click();
+		driver.findElement(By.xpath("//input[@value='option2']")).click();
+		System.out.println(driver.findElement(By.xpath("//input[@value='option2']")).isSelected());
+		Thread.sleep(3000);
+		System.out.println(driver.findElements(By.xpath("//div[@id='checkbox-example']/fieldset/label")).size());
+		driver.close();
+		*/
+		
+		
+		// Handling Radio Buttons 
+		
+		driver.findElement(By.xpath("//div[contains(text(),'Family & Friends')]")).click();		
+	}
+
+}
